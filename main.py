3fc17f9f0136e89dc67d248ebe32416ba7e479c9
echang1802 <41762304+echang1802@users.noreply.github.com>
2021-06-02 19:26:17 -0300

    Adapt main file to new version

--- main.py
+++ main.py
@@ -5,13 +5,12 @@ import pandas as pd
 if __name__ == "__main__":
 
     sim = simulator(pd.read_csv("data/test_data.csv"))
-    periods, reached = sim.simulate(users = {
-        "money_size" : 100,
-        "subsidy_size" : 100
-    }, campaigns = campaign_control(100),
-    periods_to_evolve = 2)
+    periods, reached, periods_stats = sim.simulate(campaigns = campaign_control(700),
+                periods_to_evolve = 5)
 
     if reached:
         print(f"Total periods needed to reach critical mass: {periods}")
     else:
         print(f"In {periods} periods the critical mass wasn't reached")
+
+    periods_stats.to_csv("data/periods_stats.csv", index = False)
